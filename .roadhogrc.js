68c847a5ff8b85b472f6e8f5878f3570eff16278
wuhao <wuhao@parsec.com.cn>
2017-11-14 10:25:07 +0800

    提了个交⛄️

--- .roadhogrc.js
+++ .roadhogrc.js
@@ -39,7 +39,7 @@ export default {
   theme: "./src/theme.js",
   proxy: {
     "/api": {
-      target: "http://115.29.239.213:9002/",// 正式服务器地址
+      target: "http://localhost:9002/",// 正式服务器地址
       changeOrigin: true,
       secure: false,
       pathRewrite: { "^/api": "" }
