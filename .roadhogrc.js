667dbb118d0e46e087e870e640e377b6f85fd852
Yanghc <hqwlkj@parsec.com.cn>
2017-11-14 11:15:05 +0800

    2017-11-14 启用本地代理

--- .roadhogrc.js
+++ .roadhogrc.js
@@ -37,12 +37,12 @@ export default {
   },
   ignoreMomentLocale:true,
   theme: "./src/theme.js",
-  proxy: {
-    "/api": {
-      target: "http://115.29.239.213:9002/",// 正式服务器地址
-      changeOrigin: true,
-      secure: false,
-      pathRewrite: { "^/api": "" }
-    }
-  }
+  // proxy: {
+  //   "/api": {
+  //     target: "http://115.29.239.213:9002/",// 正式服务器地址
+  //     changeOrigin: true,
+  //     secure: false,
+  //     pathRewrite: { "^/api": "" }
+  //   }
+  // }
 }
