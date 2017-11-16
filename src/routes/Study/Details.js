148b67aee9436279b058ef6deedcbdc77b86f435
Yanghc <hqwlkj@parsec.com.cn>
2017-11-16 16:58:17 +0800

    2017-11-16  新增课程图片预览功能

--- src/routes/Study/Details.js
+++ src/routes/Study/Details.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'dva';
 import {routerRedux} from 'dva/router';
 import {NavBar,Icon} from 'antd-mobile';
+import PhotoSwipe from '../../components/PhotoSwipe/index';
+import 'react-photoswiper/lib/photoswipe.css';
 import Config from '../../utils/config';
 import * as Tools from '../../utils/utils';
 import moment from 'moment';
@@ -86,6 +88,7 @@ class StudyDetails extends React.Component{
       offsetX = -26;
     }
 
+    let content = '<p><img src="http://ojiowy5mw.bkt.clouddn.com/Fuy3ZITG3Vh3FvjIc1SgSH8-ox8q" style="max-width:100%;"></p><p><img src="http://ojiowy5mw.bkt.clouddn.com/FnXzAfVUsS4--WFiYCU1YEneLc0v" style="max-width:100%;"><img src="http://ojiowy5mw.bkt.clouddn.com/FgohbvLb_YXOuZp6Pbywj52cLShZ" style="max-width: 100%;"><img src="http://ojiowy5mw.bkt.clouddn.com/FmJUrerB0Ehs6z8WAR4tQ79mO-Lo" style="max-width: 100%;"><img src="http://ojiowy5mw.bkt.clouddn.com/Fl_thKgW9fuYAh_Dh_DY8lFT4DQB" style="max-width: 100%;"><img src="http://ojiowy5mw.bkt.clouddn.com/FrlGnAh6f2wXdYfSSPDImuTeBQn-" style="max-width: 100%;"><img src="http://ojiowy5mw.bkt.clouddn.com/FksxL4O-7kmSgKCRdw6rOhGE228A" style="max-width: 100%;"><br></p><p><img src="http://ojiowy5mw.bkt.clouddn.com/FvwJR6cloNb1B69y8HoyRBKg3f-8" style="max-width:100%;"><img src="http://ojiowy5mw.bkt.clouddn.com/FrWDdCQLWLz1txHta1HUsPSbtRZW" style="max-width: 100%;"><img src="http://ojiowy5mw.bkt.clouddn.com/Fi9QvvNDMImlLZmvK8PqLf5plsFY" style="max-width: 100%;"><img src="http://ojiowy5mw.bkt.clouddn.com/Fv7BTjhuHr-oL2YLBcpLc-fQs6r2" style="max-width: 100%;"><img src="http://ojiowy5mw.bkt.clouddn.com/FoM7pR85LnWopfo7WghtkmQEb1X7" style="max-width: 100%;">知否<img src="http://ojiowy5mw.bkt.clouddn.com/FivWsjkO2gNZDrDMWqLbkvTiDzzm" style="max-width: 100%;"><img src="http://ojiowy5mw.bkt.clouddn.com/Fn2vr6PZey1hqynuLXP8xAPap5On" style="max-width: 100%;"><img src="http://ojiowy5mw.bkt.clouddn.com/Fu4DDeOieQq_cH4aV34Gaif2_WAw" style="max-width: 100%;"><img src="http://ojiowy5mw.bkt.clouddn.com/FlXqLgAdubLtVh6-lk2NUXqlObXX" style="max-width: 100%;"><img src="http://ojiowy5mw.bkt.clouddn.com/FoW2xvbnWhSBtQbLNNtTofMJmnVG" style="max-width: 100%;"><img src="http://ojiowy5mw.bkt.clouddn.com/FgPUsta56ahsZoWPSe6XxSsAFdNG" style="max-width: 100%;"><img src="http://ojiowy5mw.bkt.clouddn.com/FsM2BwieNawU8ZAzjMqwVeorzbOp" style="max-width: 100%;"><img src="http://ojiowy5mw.bkt.clouddn.com/FtYgkt8fhThwpDTXg8pkTJ4jKaDV" style="max-width: 100%;"><span style="line-height: 1.8;">&nbsp;</span></p><p><br></p>'
     let x = this.state.study.catePathName;
     let attachment = this.state.study.attachment;
     return(<div className={classnames(styles.course_details_component)}>
@@ -119,13 +122,14 @@ class StudyDetails extends React.Component{
             创建时间：<span>{moment(new Date(parseInt(this.state.study.createTime))).format('YYYY-MM-DD HH:mm:ss')}</span>
           </p> : null}
         </div>
-        <div className={styles.course_details_content} dangerouslySetInnerHTML={{__html: Tools.formatFontSize((this.state.study || {}).content)}}/>
+        {/*<div className={styles.course_details_content} dangerouslySetInnerHTML={{__html: Tools.formatFontSize((this.state.study || {}).content)}}/>*/}
+        <div className={styles.course_details_content} dangerouslySetInnerHTML={{__html: Tools.formatFontSize(content)}}/>
         {/*<div className='course-details-content'>&nbsp;*/}
         {/*{(this.state.study || {}).content}</div>*/}
       </div>
-      {/*{ !!this.state.isOpen ?*/}
-        {/*<PhotoSwipe gallerySelector={'.course-details-content'} currentId={this.state.imgIndex} options={{}}*/}
-                    {/*isOpen={this.state.isOpen} handleClose={this.handleClose.bind(this)}/> : null}*/}
+
+        <PhotoSwipe gallerySelector={'course-details-content'} currentId={this.state.imgIndex} options={{}}
+                    isOpen={this.state.isOpen} handleClose={this.handleClose.bind(this)}/>
     </div>)
   }
 }
